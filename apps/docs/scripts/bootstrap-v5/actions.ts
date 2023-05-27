2a542a3d2040c40e9a9ed2870960631ec90b354f
Thomas Ramé <thomas.rame@outlook.com>
2023-05-27 02:08:04 +0200

    chore: make bootstrap-v5 having more documentation examples

--- apps/docs/scripts/bootstrap-v5/actions.ts
+++ apps/docs/scripts/bootstrap-v5/actions.ts
@@ -1,5 +1,6 @@
 import AdmZip from 'adm-zip';
 import fs from 'fs-extra';
+import { glob } from 'glob';
 import handlebars from 'handlebars';
 import path from 'path';
 
@@ -22,38 +23,70 @@ export async function downloadAndExtract() {
 }
 
 export async function build() {
-  const srcFolderPath = path.resolve(__dirname, `../../tmp/${framework}/bootstrap-5.3.0-alpha3/site/content/docs/5.3/examples`);
-  const templateFilePath = path.resolve(__dirname, `./template.stories.ts`);
+  const entries = await glob([
+    path.resolve(__dirname, `../../tmp/${framework}/bootstrap-5.3.0-alpha3/site/content/docs/5.3`) + '/**/*.md',
+    path.resolve(__dirname, `../../tmp/${framework}/bootstrap-5.3.0-alpha3/site/content/docs/5.3/examples`) + '/**/*.html',
+  ]);
+  const folderToStripPath = path.resolve(__dirname, `../../tmp/${framework}/bootstrap-5.3.0-alpha3/site/content/docs/5.3`);
+  const storyTemplateFilePath = path.resolve(__dirname, `./template.stories.ts`);
+  const htmlTemplateFilePath = path.resolve(__dirname, `./template.hbs`);
   const outputFolderPath = path.resolve(getFrameworkFolderPath(path.resolve(__dirname, `../../`), framework), 'stories/framework/');
 
   try {
-    const entries = (await fs.readdir(srcFolderPath, { withFileTypes: true })).filter((entry) => !!entry);
-
-    const storyTemplateContent = await fs.readFile(templateFilePath, 'utf-8');
+    const storyTemplateContent = await fs.readFile(storyTemplateFilePath, 'utf-8');
     const storyTemplate = handlebars.compile(storyTemplateContent);
+    const htmlTemplateContent = await fs.readFile(htmlTemplateFilePath, 'utf-8');
+    const htmlTemplate = handlebars.compile(htmlTemplateContent);
 
     await Promise.all(
       entries.map(async (entry) => {
-        // Components are in directory, use it as the name
-        if (entry.isDirectory()) {
-          const componentName = entry.name;
+        // Subtract the root and remove the extension
+        const extension = path.extname(entry);
+        let storyName = path.relative(folderToStripPath, entry).replace(/\.[^/.]+$/, '');
 
-          const filePath = path.join(srcFolderPath, `${entry.name}/index.html`);
-          let componentHtml = await fs.readFile(filePath, 'utf-8');
+        let content = await fs.readFile(entry, 'utf-8');
+
+        if (extension === '.html') {
+          // They all end with `/meaningful/index.html`
+          storyName = path.dirname(storyName);
 
           // Remove meta content at the top that is visible when rendering the story and escape needed quotes for the template
-          componentHtml = componentHtml
+          const pageHtml = content
             .replace(/---[\s\S]*?---/, '')
             .replace(/\{\{[\s\S]*?\}\}/g, '') // Remove all templating pleceholders of the Bootstrap documentation
             .trim();
 
           // Format the story
           const storyContent = storyTemplate({
-            component: componentName,
+            component: storyName,
           });
 
-          await fs.outputFile(path.join(outputFolderPath, componentName, 'index.html'), componentHtml);
-          await fs.outputFile(path.join(outputFolderPath, componentName, 'index.stories.ts'), storyContent);
+          await fs.outputFile(path.join(outputFolderPath, storyName, 'index.html'), pageHtml);
+          await fs.outputFile(path.join(outputFolderPath, storyName, 'index.stories.ts'), storyContent);
+        } else {
+          // Get all code sections
+          // Note: the regex looks for code between specific tags, but skip those having another tags inside
+          // because it's likely it's for the templating of Bootstrap (for loop...) and it's not worth it to have those specific cases
+          const regex = /{{< example(?:[^>]*)? >}}((?:(?!{{<)[\s\S])*){{< \/example >}}/g;
+          const matches = content.matchAll(regex);
+          const codes = Array.from(matches).map((m) => {
+            return m[1].trim();
+          });
+
+          if (!codes.length) {
+            return;
+          }
+
+          // Format the story
+          const storyContent = storyTemplate({
+            component: storyName,
+          });
+          const pageHtml = htmlTemplate({
+            codes: codes,
+          });
+
+          await fs.outputFile(path.join(outputFolderPath, storyName, 'index.html'), pageHtml);
+          await fs.outputFile(path.join(outputFolderPath, storyName, 'index.stories.ts'), storyContent);
         }
       })
     );
