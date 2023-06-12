70ae267a5c6b3e5382317873a99a85c49d18ef54
Thomas Ramé <thomas.rame@outlook.com>
2023-06-12 17:11:26 +0200

    chore: provide all js exports for the ease of usage

--- packages/dsfr-connect/src/index.ts
+++ packages/dsfr-connect/src/index.ts
@@ -1,3 +1,13 @@
+import * as common from '@dsfrc/dsfr-connect/src/common';
+import * as muiV5 from '@dsfrc/dsfr-connect/src/mui-v5';
+import * as storybookV7 from '@dsfrc/dsfr-connect/src/storybook-v7';
+import * as vuetifyV3 from '@dsfrc/dsfr-connect/src/vuetify-v3';
+
 export default {
+  common: common,
   bootstrapV5: {},
+  fonts: {},
+  vuetifyV3: vuetifyV3,
+  muiV5: muiV5,
+  storybookV7: storybookV7,
 };
